78d67e2ab004889b5ac061b09b0e6d42ce57185a
Pavel Simovec <psimovec@redhat.com>
2020-05-10 05:00:26 +0200

    suicide

--- src/components/CoronaChart.js
+++ src/components/CoronaChart.js
@@ -1,96 +1,49 @@
 import React from "react";
-import { Chart, ChartAxis, ChartGroup, ChartLine, ChartVoronoiContainer } from "@patternfly/react-charts";
+import {
+    Chart,
+    ChartAxis,
+    ChartGroup,
+    ChartLine,
+    ChartThemeColor,
+    ChartVoronoiContainer
+} from "@patternfly/react-charts";
 
-const CoronaChart = ({ koronky }) => <div style={{ height: "250px", width: "600px" }}>
-  <Chart
-    ariaDesc="Average number of pets"
-    ariaTitle="Line chart example"
-    containerComponent={<ChartVoronoiContainer labels={({ datum }) => `${datum.name}: ${datum.y}`} constrainToVisibleArea />}
-    // legendData={[{ name: 'Cats' }, { name: 'Dogs', symbol: { type: 'dash' } }, { name: 'Birds' }, { name: 'Mice' }]}
-    legendData={koronky.map((x) => { console.log("x je", x);
-      return { name: x[0] };
-    })}
-    legendOrientation="vertical"
-    legendPosition="right"
-    height={250}
-    maxDomain={{ y: 300000 }}
-    minDomain={{ y: 0 }}
-    padding={{
-      bottom: 50,
-      left: 50,
-      right: 200, // Adjusted to accommodate legend
-      top: 50
-    }}
-    width={600}
-  >
-    {/*<ChartAxis tickValues={[2, 3, 4]} />*/}
-    {/*<ChartAxis dependentAxis showGrid tickValues={[2, 5, 8]} />*/}
-    {koronky[0] &&
-    <ChartGroup>
-      {/*{koronky.map((country) => {*/}
-      {/*      return <ChartLine*/}
-      {/*          data={country[1].map(entry => {*/}
-      {/*            return {name: country[0], x: entry["date"], y: entry["confirmed"]}*/}
-      {/*          })}*/}
-      {/*      />*/}
-      {/*    }*/}
-      {/*)};*/}
-      {/*<ChartLine*/}
-      {/*  data={[*/}
-      {/*    { name: "Cats", x: "2015", y: 1 },*/}
-      {/*    { name: "Cats", x: "2016", y: 2 },*/}
-      {/*    { name: "Cats", x: "2017", y: 5 },*/}
-      {/*    { name: "Cats", x: "2018", y: 3 }*/}
-      {/*  ]}*/}
-      {/*/>*/}
-      <ChartLine
-          data={[{name: "Czechia", x: "2015-01-06", y: 10}, {name: "Czechia", x: "2015-01-07", y: 10}]}
-      />
+const CoronaChart = ({ koronky }) =>
+    <Chart
+      ariaDesc="Average number of pets"
+      ariaTitle="Line chart example"
+      containerComponent={<ChartVoronoiContainer labels={({ datum }) => {return `${datum.name}: ${datum.y} at ${datum.x}`}} constrainToVisibleArea />}
+      // legendData={[{ name: 'Cats' }, { name: 'Dogs', symbol: { type: 'dash' } }, { name: 'Birds' }, { name: 'Mice' }]}
+      legendData={koronky.map((x) => {
+        return { name: x[0] };
+      })}
+      legendOrientation="vertical"
+      legendPosition="right"
+      themeColor={ChartThemeColor.multiUnordered}
+      // maxDomain={{ y: 10000 }}
+      // minDomain={{ y: 0 }}
+      padding={{top:50,
+        left: 75,
+        right: 150, // Adjusted to accommodate legend
+          bottom:50,
+      }}
+    >
+      {/*<ChartAxis dependentAxis tickValues={[0,1,2,3,4,5].map((x)=>x*2000)} />*/}
+        {/*<ChartAxis tickValues={[2, 3, 4]} />*/}
+      {/*<ChartAxis tickValues={[ koronky[0][1][0]['date'], koronky[0][1][50]['date'], koronky[0][1].last()['date']]} />*/}
+        <ChartAxis tickValues={ [koronky[0][1][0]['date'], koronky[0][1][koronky[0][1].length-1]['date']]} />
       {koronky[0] &&
-      <ChartLine
-          data={koronky[0][1].map(entry => {
-            return {name: koronky[0][0], x: entry["date"], y: entry["confirmed"]}
-          })}
-      />}
-      {koronky.map(koronka =>
-      {return koronka &&
-      <ChartLine
-          data={koronka[1].map(entry => {
-            return {name: koronka[0], x: entry["date"], y: entry["confirmed"]}
-          })}
-      />})}
-      {/*    <ChartLine*/}
-      {/*        data={[*/}
-      {/*            { name: 'Dogs', x: '2015', y: 2 },*/}
-      {/*            { name: 'Dogs', x: '2016', y: 1 },*/}
-      {/*            { name: 'Dogs', x: '2017', y: 7 },*/}
-      {/*            { name: 'Dogs', x: '2018', y: 4 }*/}
-      {/*        ]}*/}
-      {/*        style={{*/}
-      {/*            data: {*/}
-      {/*                strokeDasharray: '3,3'*/}
-      {/*            }*/}
-      {/*        }}*/}
-      {/*    />*/}
-      {/*    <ChartLine*/}
-      {/*        data={[*/}
-      {/*            { name: 'Birds', x: '2015', y: 3 },*/}
-      {/*            { name: 'Birds', x: '2016', y: 4 },*/}
-      {/*            { name: 'Birds', x: '2017', y: 9 },*/}
-      {/*            { name: 'Birds', x: '2018', y: 5 }*/}
-      {/*        ]}*/}
-      {/*    />*/}
-      {/*    <ChartLine*/}
-      {/*        data={[*/}
-      {/*            { name: 'Mice', x: '2015', y: 3 },*/}
-      {/*            { name: 'Mice', x: '2016', y: 3 },*/}
-      {/*            { name: 'Mice', x: '2017', y: 8 },*/}
-      {/*            { name: 'Mice', x: '2018', y: 7 }*/}
-      {/*        ]}*/}
-      {/*    />*/}
-    </ChartGroup>
-    }
-  </Chart>
-</div>;
+      <ChartGroup>
+
+        {koronky.map(koronka =>
+        {return koronka &&
+        <ChartLine
+            data={koronka[1].map(entry => {
+              return {name: koronka[0], x: entry["date"], y: entry["confirmed"]}
+            })}
+        />})}
+      </ChartGroup>
+      }
+    </Chart>;
 
 export default CoronaChart;
--- src/components/Tabulka.js
+++ src/components/Tabulka.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Table, TableHeader, TableBody, sortable, SortByDirection, wrappable } from "@patternfly/react-table";
 
 const headerTemplate = ["Country", "confirmed today", "confirmed before week", "~weekly", "~weekly%", "koronarip"];
-
-const Tabulka = ({ rows, onSort, ...other }) => {
+const Tabulka = ({ rows, onSort, onSelect, selected, ...other }) => {
+    const srows = rows.map((row) => {row.selected = selected.has(row[0]); return row})
   return (
     <Table
       aria-label="Sortable coronavirus table"
       cells={headerTemplate.map((x) => {
         return { title: x, transforms: [sortable, wrappable] };
       })}
-      rows={rows}
+      rows={srows}
       onSort={onSort}
-      style={{float:"right"}}
+      onSelect={onSelect}
+      canSelectAll={false}
+      style={{inlineBlock:"right"}}
         {...other}
     >
       <TableHeader />
